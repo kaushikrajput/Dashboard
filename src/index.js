1ce63425975860fa36badba67ae42c411991377d
kaushik <kaushikrajput0926@gmail.com>
2023-05-18 22:34:32 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ContextProvider } from "./contexts/ContextProvider";
-import { AuthContextProvider } from "./contexts/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <ContextProvider>
-      <AuthContextProvider>
         <App />
-      </AuthContextProvider>
     </ContextProvider>
   </React.StrictMode>
 );
