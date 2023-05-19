28468d157cebf7f2622b13d3c35aa196a23b58bc
kaushik <kaushikrajput0926@gmail.com>
2023-05-19 12:24:14 +0530

    initial commit

--- src/Users/Login.jsx
+++ src/Users/Login.jsx
@@ -1,99 +1,99 @@
-import React, { useRef, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase";
+// import React, { useRef, useState } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import { signInWithEmailAndPassword } from "firebase/auth";
+// import { auth } from "../firebase";
 
-const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
+// const Login = () => {
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [error, setError] = useState(false);
+//   const [loading, setLoading] = useState(false);
+//   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
 
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        // dispatch({type:"LOGIN", payload:user})
-        navigate("/")
-      })
-      .catch((error) => {
-        setError(true);
-      });
-  };
+//     signInWithEmailAndPassword(auth, email, password)
+//       .then((userCredential) => {
+//         const user = userCredential.user;
+//         // dispatch({type:"LOGIN", payload:user})
+//         navigate("/")
+//       })
+//       .catch((error) => {
+//         setError(true);
+//       });
+//   };
 
-  return (
-    <>
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 w-400">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Login
-            </h1>
-            {error && (
-              <div
-                className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
-                role="alert"
-              >
-                {error}
-              </div>
-            )}
-            <form
-              className="space-y-4 md:space-y-6"
-              action="#"
-              onSubmit={handleSubmit}
-            >
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Email
-                </label>
-                <input
-                  onChange={(e) => setEmail(e.target.value)}
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
-                <input
-                  onChange={(e) => setPassword(e.target.value)}
-                  type="password"
-                  name="password"
-                  id="password"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full  p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
-              </div>
+//   return (
+//     <>
+//       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 w-400">
+//         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+//           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+//             <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+//               Login
+//             </h1>
+//             {error && (
+//               <div
+//                 className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+//                 role="alert"
+//               >
+//                 {error}
+//               </div>
+//             )}
+//             <form
+//               className="space-y-4 md:space-y-6"
+//               action="#"
+//               onSubmit={handleSubmit}
+//             >
+//               <div>
+//                 <label
+//                   htmlFor="email"
+//                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+//                 >
+//                   Email
+//                 </label>
+//                 <input
+//                   onChange={(e) => setEmail(e.target.value)}
+//                   type="email"
+//                   name="email"
+//                   id="email"
+//                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+//                   required=""
+//                 />
+//               </div>
+//               <div>
+//                 <label
+//                   htmlFor="password"
+//                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+//                 >
+//                   Password
+//                 </label>
+//                 <input
+//                   onChange={(e) => setPassword(e.target.value)}
+//                   type="password"
+//                   name="password"
+//                   id="password"
+//                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full  p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+//                   required=""
+//                 />
+//               </div>
 
-              <button
-                type="submit"
-                className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                Login
-              </button>
-            </form>
-          </div>
-        </div>
+//               <button
+//                 type="submit"
+//                 className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+//               >
+//                 Login
+//               </button>
+//             </form>
+//           </div>
+//         </div>
 
-        <div className="w-100 text-center mt-2">
-          Need an account? <Link to="/signup"> Sign up</Link>
-        </div>
-      </div>
-    </>
-  );
-};
+//         <div className="w-100 text-center mt-2">
+//           Need an account? <Link to="/signup"> Sign up</Link>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
 
-export default Login;
+// export default Login;
--- src/Users/SignUp.jsx
+++ src/Users/SignUp.jsx
@@ -1,101 +1,101 @@
-import React, { useRef, useState } from "react";
-import { Form, Button, Card, Alert } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+// import React, { useRef, useState } from "react";
+// import { Form, Button, Card, Alert } from "react-bootstrap";
+// import { Link, useNavigate } from "react-router-dom";
 
-const SignUp = () => {
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const passwordConfirmRef = useRef();
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
+// const SignUp = () => {
+//   const emailRef = useRef();
+//   const passwordRef = useRef();
+//   const passwordConfirmRef = useRef();
+//   const [error, setError] = useState("");
+//   const [loading, setLoading] = useState(false);
 
-  const handleSubmit =  (e) => {
-    e.preventDefault();
-    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-      return setError("Passwords do not match");
-    }
+//   const handleSubmit =  (e) => {
+//     e.preventDefault();
+//     if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+//       return setError("Passwords do not match");
+//     }
 
-    try {
-      setError("");
-      setLoading(true);
-       (emailRef.current.value, passwordRef.current.value);
-    } catch {
-      setError("Failed to create an account");
-    }
-    setLoading(false);
-  };
+//     try {
+//       setError("");
+//       setLoading(true);
+//        (emailRef.current.value, passwordRef.current.value);
+//     } catch {
+//       setError("Failed to create an account");
+//     }
+//     setLoading(false);
+//   };
 
-  return (
-    <>
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 w-400">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              SignUp
-            </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full  p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="confirm-password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password Confirmation
-                </label>
-                <input
-                  type="confirm-password"
-                  name="confirm-password"
-                  id="confirm-password"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full  p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
-              </div>
+//   return (
+//     <>
+//       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 w-400">
+//         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+//           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+//             <h1 className="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+//               SignUp
+//             </h1>
+//             <form className="space-y-4 md:space-y-6" action="#">
+//               <div>
+//                 <label
+//                   htmlFor="email"
+//                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+//                 >
+//                   Email
+//                 </label>
+//                 <input
+//                   type="email"
+//                   name="email"
+//                   id="email"
+//                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+//                   required=""
+//                 />
+//               </div>
+//               <div>
+//                 <label
+//                   htmlFor="password"
+//                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+//                 >
+//                   Password
+//                 </label>
+//                 <input
+//                   type="password"
+//                   name="password"
+//                   id="password"
+//                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full  p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+//                   required=""
+//                 />
+//               </div>
+//               <div>
+//                 <label
+//                   htmlFor="confirm-password"
+//                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+//                 >
+//                   Password Confirmation
+//                 </label>
+//                 <input
+//                   type="confirm-password"
+//                   name="confirm-password"
+//                   id="confirm-password"
+//                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full  p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+//                   required=""
+//                 />
+//               </div>
 
-              <button
-                type="submit"
-                className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                Sign Up
-              </button>
-            </form>
-          </div>
-        </div>
+//               <button
+//                 type="submit"
+//                 className="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+//               >
+//                 Sign Up
+//               </button>
+//             </form>
+//           </div>
+//         </div>
 
-        <div className="w-100 text-center mt-2">
-          Already have an account? <Link to="/login"> Log in</Link>
-        </div>
-      </div>
-    </>
-  );
-};
+//         <div className="w-100 text-center mt-2">
+//           Already have an account? <Link to="/login"> Log in</Link>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
 
-export default SignUp;
+// export default SignUp;
